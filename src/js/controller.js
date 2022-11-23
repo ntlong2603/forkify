2c09b56d3d2c3c5befc4ffd2c40f1e1a1abc9a78
longnguyen <ntlong2603@gmail.com>
2022-11-23 17:20:07 +0700

    update

--- src/js/controller.js
+++ src/js/controller.js
@@ -51,7 +51,6 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query)
 
     // 3) Render results
-    // resultsView.render(model.state.search.results)
     resultsView.render(model.getSearchResultsPage())
 
     // 4) Render initial panigation buttons
